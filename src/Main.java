d6aaf084c5293c4f49984eac54d61c8d7667fbac
ali-erdem <alierdemcaglar@hotmail.com>
2022-09-30 01:00:14 +0300

    patika

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,7 @@ public class Main {
     
     public static void main(String[]args){
        
-        String firstName = "Ali Erdem";
-        System.out.println(firstName.charAt(0));
-        System.out.println(firstName.toUpperCase());
-        System.out.println(firstName.toLowerCase());
-        System.out.println(firstName.replace('e', 'a'));
-        System.out.println(firstName.toUpperCase().replace('E', 'o'));
-        System.out.println(firstName.length());
-        System.out.println(firstName.substring(2));
+      
         
     }
 
